2e3533c4b6e0a205e5618568f33209ebaaff7e48
Jack Reed <phillipjreed@gmail.com>
2018-10-18 14:33:20 -0700

    add a test file and try this thing w/ async

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const vision = require('@google-cloud/vision');
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
-exports.helloIiif = (req, res) => {
+exports.helloIiif = async(req, res) => {
   // Performs label detection on the image file
   console.log(req);
-  console.log(req.body.url);
-  try {
-    const results = Promise.resolve(client.labelDetection(req.body.url));
-    const labels = results[0].labelAnnotations;
-    res.send('Labels:');
-    res.send(labels.map(label => label.description));
-  } catch(err) {
-    console.error(err);
-  }
+  console.log(req.body.url)
+  await client
+    .labelDetection(req.body.url)
+    .then(results => {
+      const labels = results[0].labelAnnotations;
+
+      res.send('Labels:');
+      res.send(labels.map(label => label.description));
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
 }
---
+++ test.js
@@ -0,0 +1,4 @@
+const helloIiif = require('./index.js');
+
+// console.log(helloIiif);
+helloIiif.helloIiif({body: {url: 'https://raw.githubusercontent.com/GoogleCloudPlatform/cloud-vision/master/data/text/wakeupcat.jpg'}})
