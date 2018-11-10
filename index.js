e8b615188603391414f0b0b50476c1e5ebc8b549
Jessie Keck <jessie.keck@gmail.com>
2018-11-09 16:03:04 -0800

    Store data returned from Google Cloud Vision to a Firestore database.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
+const Firestore = require('@google-cloud/firestore');
 const manifesto = require('manifesto.js');
+const crypto = require('crypto');
 
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
+
+// Creates a Firestore db
+const db = new Firestore();
 
 exports.helloIiif = async(req, res) => {
   // Performs label detection on the image file
@@ -22,10 +27,32 @@ exports.helloIiif = async(req, res) => {
   const foo = manifestation.getSequences()[0].getCanvases()[0].getCanonicalImageUri(width);
   console.log(foo);
   await client
-    .labelDetection(foo)
+    .annotateImage(
+      {
+        features: [
+          { type: 'TYPE_UNSPECIFIED' },
+          { type: 'FACE_DETECTION' },
+          { type: 'LANDMARK_DETECTION' },
+          { type: 'LOGO_DETECTION' },
+          { type: 'LABEL_DETECTION' },
+          { type: 'DOCUMENT_TEXT_DETECTION' },
+          { type: 'SAFE_SEARCH_DETECTION' },
+          { type: 'IMAGE_PROPERTIES' },
+          { type: 'CROP_HINTS' },
+          { type: 'WEB_DETECTION' }
+        ],
+        image: { source: { imageUri: foo } }
+      }
+    )
     .then(results => {
       console.log(results);
       const labels = results[0].labelAnnotations;
+
+      var iiifDoc = db.collection('iiifCloudVisionStuff').doc(
+        crypto.createHash('md5').update(req.body.url).digest('hex')
+      );
+
+      iiifDoc.set(JSON.parse(JSON.stringify(results))[0]);
 
       res.send(labels.map(label => label.description));
     })
